d1c9f7872889d7b5666d92fce6fdf9c1f975af56
Ben Dale <ben@ben-dale.co.uk>
2019-11-14 20:56:37 +0000

    remove timeout

--- log_temperature.py
+++ log_temperature.py
@@ -13,7 +13,7 @@ working_dir = os.path.dirname(os.path.realpath(__file__))
 if not os.path.exists(working_dir + '/data'):
     os.makedirs(working_dir + '/data')
 
-ser = Serial(sys.argv[1], 9600, timeout=5)
+ser = Serial(sys.argv[1], 9600)
 temperature = ser.readline().decode('utf-8').strip(' \t\n\r')
 date = datetime.now().strftime('%d-%m-%Y')
 time = datetime.now().strftime('%H:%M')
